2922755cbc75fd0e6d39699a3702e92a88c21c86
Betanine409 <73406253+Betanine409@users.noreply.github.com>
2021-06-09 14:40:47 -0700

    Some new functionality
    I'm still too lazy to put anything here

--- 2048.py
+++ 2048.py
@@ -1,10 +1,20 @@
 import pygame
+import random
 import os
+
+
 pygame.init()
 
-# Variables
+# Main Variables
+WIDTH = 1000
+HEIGHT = 1000
+tile_gotten = False
+
+layers = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+# Tile Variables
 clock = pygame.time.Clock()
-window = pygame.display.set_mode([1000, 1000])
+window = pygame.display.set_mode([WIDTH, HEIGHT])
 TWO = pygame.image.load(os.path.join('2048', 'Assets', '2.png'))
 FOUR = pygame.image.load(os.path.join('2048', 'Assets', '4.png'))
 EIGHT = pygame.image.load(os.path.join('2048', 'Assets', '8.png'))
@@ -17,7 +27,12 @@ FIVEHUNDREDTWELVE = pygame.image.load(os.path.join('2048', 'Assets', '512.png'))
 ONETHOUSANDTWENTYFOUR = pygame.image.load(os.path.join('2048', 'Assets', '1024.png'))
 TWOTHOUSANDFOURTYEIGHT = pygame.image.load(os.path.join('2048', 'Assets', '2048.png'))
 
+
+# Outline variables
+# OUTLINE = pygame.Rect(WIDTH / 2, HEIGHT / 2, 400, 400)
+
 WHITE = (255, 255, 255)
+# OUTLINE_COLOR = (107, 107, 107)
 
 def DRAW_WINDOW():
     window.fill(WHITE)
@@ -33,8 +48,30 @@ def DRAW_WINDOW():
     window.blit(ONETHOUSANDTWENTYFOUR, (900, 0))
     window.blit(TWOTHOUSANDFOURTYEIGHT, (0, 100))
     
+    # pygame.draw.rect(window, OUTLINE_COLOR, OUTLINE)
     
     pygame.display.update()
+
+
+def tile_generation():
+    tile_gotten = False
+    if 0 in layers[0] or 0 in layers[1] or 0 in layers[2] or 0 in layers[3]:
+        while not tile_gotten:
+            layer_position = random.randint(0, 3)
+            tile_position = random.randint(0, 3)
+            if layers[layer_position][tile_position] == 0:
+                generate_number = random.randint(1, 4)
+                if generate_number <= 3:
+                    layers[layer_position][tile_position] = 2
+                else:
+                    layers[layer_position][tile_position] = 4
+                tile_gotten = True
+    else:
+        print("The board is full")
+def UP():
+    tile_generation()
+    print(layers)
+
 
 def main():
     running = True
@@ -44,7 +81,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+                pygame.QUIT()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    UP()
+                if event.key == pygame.K_a:
+                    LEFT()
+                if event.key == pygame.K_s:
+                    DOWN()
+                if event.key == pygame.K_d:
+                    RIGHT()
         DRAW_WINDOW()
 
 
